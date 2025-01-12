d3133b85b66ac12ed175e7149ff439c30a2fcfb6
Erin McLaughlin <sa-em5mb@virginia.edu>
2025-01-12 12:26:48 -0500

    remove apiservice project

--- src/ApiService/Database/BlinkDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace Blink.ApiService.Database;
-
-public sealed class BlinkDbContext(DbContextOptions<BlinkDbContext> options) : DbContext(options)
-{
-    public DbSet<BlinkUser> Users => Set<BlinkUser>();
-}
--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -3,21 +3,11 @@ using Blink.AppHost;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgresServer = builder.AddDefaultPostgresServer();
+var pgDatabase = postgresServer.AddDatabase("blink-db2");
 
-var pgDatabase = postgresServer.AddDatabase("blink-db");
-var pgDatabase2 = postgresServer.AddDatabase("blink-db2");
-
-var apiService = builder
-    .AddProject<Projects.ApiService>("blink-api")
+builder
+    .AddProject<Projects.WebApp>("webapp")
     .WithReference(pgDatabase)
     .WaitFor(pgDatabase);
-
-var blinkUi = builder
-    .AddProject<Projects.Web>("blink-ui")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
-builder.AddProject<Projects.WebApp>("webapp").WithReference(pgDatabase2).WaitFor(pgDatabase2);
 
 builder.Build().Run();
